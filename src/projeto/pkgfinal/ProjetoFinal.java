7484127710c92bcc7a8eb46d95149ea9d04f6ba0
Fernanda <fernandasousac159@gmail.com>
2021-07-08 20:33:39 -0300

    Segundo commit

--- src/projeto/pkgfinal/ProjetoFinal.java
+++ src/projeto/pkgfinal/ProjetoFinal.java
@@ -15,7 +15,8 @@ public class ProjetoFinal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Projeto().setVisible(true);
+        new TelaLogin().setVisible(true);
+        
     }
     
 }
